64433735db2f049062e2379ce91bbec6de7fcfbd
seongjin_song <seongjin_song@tmax.co.kr>
2022-09-27 17:14:19 +0900

    refactor : gameview booting 상태 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,7 +8,9 @@ public class Application {
         // TODO: 프로그램 구현
         GameView gameview = new GameView();
         while (!"exit".equals(gameview.getState())) {
-            if ("start".equals(gameview.getState())) {
+            if ("booting".equals(gameview.getState())) {
+                gameview.receiveArgs();
+            }else if("in-game".equals(gameview.getState())){
                 gameview.receiveInGame();
             }else if("end".equals(gameview.getState())){
                 gameview.receiveOption();
--- src/main/java/baseball/view/GameView.java
+++ src/main/java/baseball/view/GameView.java
@@ -3,25 +3,37 @@ package baseball.view;
 import baseball.model.BaseBallNumber;
 import camp.nextstep.edu.missionutils.*;
 public class GameView {
-    private String state = "start";
+    private String state = "booting";
     private BaseBallNumber baseBallNumber;
     public GameView(){
         baseBallNumber = new BaseBallNumber();
     }
-    public String receiveInGame() {
+    public void receiveArgs() {
+        String result = Console.readLine();
+        checkValid(result);
+        baseBallNumber = new BaseBallNumber(result);
+        state = "in-game";
+    }
+    public void receiveInGame() {
         System.out.println("숫자를 입력해주세요 :");
         String result = Console.readLine();
-        if(!BaseBallNumber.isValid(result)) {
-            state = "exit";
-            throw new IllegalArgumentException("invalid baseball number");
+        if (checkGameOptionInput(result)) {
+            return;
         }
+        checkValid(result);
         String response = response(baseBallNumber.check(result));
         System.out.println(response);
         if("3스트라이크".equals(response)){
             state="end";
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
         }
-        return result;
+    }
+
+    private void checkValid(String result) {
+        if(!BaseBallNumber.isValid(result)) {
+            state = "exit";
+            throw new IllegalArgumentException("invalid baseball number");
+        }
     }
 
     public String response(String input){
@@ -37,17 +49,25 @@ public class GameView {
     public void receiveOption(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String result = Console.readLine();
-        if("1".equals(result)){
-            baseBallNumber = new BaseBallNumber();
-            state = "start";
-            return;
-        }
-        if("2".equals(result)){
-            state = "exit";
+        if (checkGameOptionInput(result)) {
             return;
         }
         throw new IllegalArgumentException("invalid option");
     }
+
+    private boolean checkGameOptionInput(String result) {
+        if("1".equals(result)){
+            baseBallNumber = new BaseBallNumber();
+            state = "booting";
+            return true;
+        }
+        if("2".equals(result)){
+            state = "exit";
+            return true;
+        }
+        return false;
+    }
+
     public String getState() {
         return state;
     }
